e984fe1dbb1479885ac077fffb349639f0005a3d
Vulcan <vulcan.lee@gmail.com>
2022-07-18 15:20:34 +0800

    在多執行緒下分別對於的集合型別物件存取 - 使用 foreach 與 for

---
+++ MultipleThreadAccessCollection/MultipleThreadAccessCollection/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace MultipleThreadAccessCollection
+{
+    using static System.Console;
+
+    internal class Program
+    {
+        static List<string> list = new List<string>();
+        static void Main(string[] args)
+        {
+            #region 執行緒 1
+            new Thread(() =>
+            {
+                while (true)
+                { list.Add("A"); Write("A"); }
+            })
+            { IsBackground = true }.Start();
+            #endregion
+
+            #region 執行緒 2
+            //new Thread(() =>
+            //{
+            //    while (true)
+            //        foreach (var item in list) { Write("R"); }
+            //    list.Clear();
+            //    Write("C");
+            //})
+            //{ IsBackground = true }.Start();
+            #endregion
+
+            #region 執行緒 3
+            new Thread(() =>
+            {
+                while (true)
+                    for (int i = 0; i < list.Count; i++)
+                    { var b = list[i]; Write("R"); }
+                list.Clear();
+                Write("C");
+            })
+            { IsBackground = true }.Start();
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MultipleThreadAccessCollection/MultipleThreadAccessCollection/原始問題程式碼.cs
@@ -0,0 +1,62 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace MultipleThreadAccessCollection
+//{
+//    internal class Program
+//    {
+//        static List<string> list = new List<string>();
+//        static void Main(string[] args)
+//        {
+
+//            new Thread(() =>
+//            {
+//                Run1();
+//            })
+//            { IsBackground = true }.Start();
+//            new Thread(() =>
+//            {
+//                Run2();
+//                //Run3();
+//            })
+//            { IsBackground = true }.Start();
+
+//            Console.ReadKey();
+//        }
+
+
+//        static void Run1()
+//        {
+//            while (true)
+//            {
+//                list.Add("A");
+//            }
+//        }
+//        static void Run2()
+//        {
+//            while (true)
+//            {
+//                foreach (var item in list)
+//                {
+
+//                }
+//                list.Clear();
+//            }
+//        }
+
+//        static void Run3()
+//        {
+//            while (true)
+//            {
+//                for (int i = 0; i < list.Count; i++)
+//                {
+//                    var b = list[i];
+//                }
+//                list.Clear();
+//            }
+//        }
+//    }
+//}
