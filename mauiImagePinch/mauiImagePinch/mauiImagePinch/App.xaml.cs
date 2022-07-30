9a3aced6c5b77b04eb002c934223f3731b21542c
Vulcan <vulcan.lee@gmail.com>
2022-07-30 14:44:45 +0800

    使用 ScrollView Image 做到圖片縮放的效果

---
+++ mauiImagePinch/mauiImagePinch/mauiImagePinch/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace mauiImagePinch;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+}
---
+++ mauiImagePinch/mauiImagePinch/mauiImagePinch/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿namespace mauiImagePinch.ViewModels;
+
+using System.ComponentModel;
+using Prism.Events;
+using Prism.Navigation;
+using Prism.Services;
+public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private readonly INavigationService navigationService;
+    public double OriginalWidth { get; set; } = 300;
+    public double OriginalHeigh { get; set; } = 300;
+    public double CurrentWidth { get; set; } = 300;
+    public double CurrentHeigh { get; set; } = 300;
+    public double Ratio { get; set; } = 1;
+    public DelegateCommand ResizeImageCommand { get; set; }
+    public MainPageViewModel(INavigationService navigationService)
+    {
+        this.navigationService = navigationService;
+        RefreshImage();
+
+        ResizeImageCommand = new DelegateCommand(() =>
+        {
+            RefreshImage();
+        });
+    }
+
+    void RefreshImage()
+    {
+        CurrentWidth = OriginalWidth * Ratio;
+        CurrentHeigh = OriginalHeigh * Ratio;
+    }
+
+    public void OnNavigatedFrom(INavigationParameters parameters)
+    {
+    }
+
+    public void OnNavigatedTo(INavigationParameters parameters)
+    {
+    }
+
+}
