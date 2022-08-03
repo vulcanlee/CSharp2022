8e1177f3231da130f53f2aeed97ad7320305cfd0
Vulcan <vulcan.lee@gmail.com>
2022-08-03 14:14:23 +0800

    Get Started with DevExpress Popup for .NET MAUI

---
+++ mauiDevExpressPopup/mauiDevExpressPopup/mauiDevExpressPopup/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.Maui;
+
+namespace mauiDevExpressPopup;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UsePrismApp<App>(PrismStartup.Configure)
+            // This method adds handlers for all DevExpress components.
+            .UseDevExpress()
+            /* You can also use the code below to add the DXPopup handler.
+            .ConfigureMauiHandlers(handlers => {
+                handlers.AddHandler<DXPopup, DXPopupHandler>();
+            })
+            */
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        return builder.Build();
+    }
+}
