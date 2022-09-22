43dc33ae10ff5288eab9c8c7b0e73a869dcdffa6
Vulcan <vulcan.lee@gmail.com>
2022-09-22 13:30:01 +0800

    在 .NET C# 下使用 MQTT 來訂閱與發佈訊息

--- csMqtt/csMqttClient/Program.cs
+++ csMqtt/csMqttClient/Program.cs
@@ -12,39 +12,44 @@ internal class Program
 
     static void Main(string[] args)
     {
-        // Create client
+        // 使用 MqttFactory 工廠類別，建立一個 MQTT 用戶端的物件
         _mqttClient = new MqttFactory().CreateMqttClient();
+
+        // 每個用戶端都要有唯一的 Id，並且要再宣告 MQTT 伺服器的連線位址與方式
         var options = new MqttClientOptionsBuilder().WithClientId("MqttClient")
                                                     .WithTcpServer("localhost", 1884)
                                                     .Build();
-        // When client connected to the server
+
+        // 綁定當此用戶端已經連線到遠端伺服器之後，所要處理的工作
         _mqttClient.UseConnectedHandler(async e =>
         {
-            // Subscribe to a topic
-            MqttClientSubscribeResult subResult = await _mqttClient.SubscribeAsync(new MqttClientSubscribeOptionsBuilder()
-                                                                   .WithTopicFilter("mqttServerTopic")
-                                                                   .Build());
-            // Sen a test message to the server
-            PublishMessage("Test Message");
+            // 在此訂閱有興趣的主題訊息
+            MqttClientSubscribeResult subResult =
+            await _mqttClient.SubscribeAsync(new MqttClientSubscribeOptionsBuilder()
+            .WithTopicFilter("HiEcho")
+            .Build());
+
+            // 對伺服器送出問好訊息
+            PublishMessage("Hi", "Test Message");
         });
 
-        // When client received a message from server
+        // 綁定當訊息傳送到此用戶端之後，將會觸發的事件
         _mqttClient.UseApplicationMessageReceivedHandler(e =>
         {
-            Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+            Console.WriteLine($"來自伺服器的問安 {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
         });
 
-        // Connect ot server
+        // 連線到遠端伺服器上
         _mqttClient.ConnectAsync(options, CancellationToken.None);
 
         Console.Read();
     }
 
-    private static async void PublishMessage(string message)
+    private static async void PublishMessage(string topic, string message)
     {
         // Create mqttMessage
         var mqttMessage = new MqttApplicationMessageBuilder()
-                            .WithTopic("mqttServerTopic")
+                            .WithTopic(topic)
                             .WithPayload(message)
                             .WithExactlyOnceQoS()
                             .Build();
