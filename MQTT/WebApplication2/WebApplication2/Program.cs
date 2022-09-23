5efe92d2b51c030808beb26ec2c21ffcce070efc
Vulcan <vulcan.lee@gmail.com>
2022-09-23 23:43:10 +0800

    測試

--- MQTT/WebApplication2/WebApplication2/Program.cs
+++ MQTT/WebApplication2/WebApplication2/Program.cs
@@ -1,12 +1,20 @@
 using MQTTnet.AspNetCore;
+using MQTTnet.Diagnostics;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(o =>
+{
+    o.ListenAnyIP(1884, l => l.UseMqtt());
+    o.ListenAnyIP(5059);
+    o.ListenAnyIP(7059);
+});
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 // This adds a hosted mqtt server to the services
-builder.Services.AddHostedMqttServer(mqttServer => mqttServer.WithoutDefaultEndpoint())
+builder.Services.AddHostedMqttServer(mqttServer => mqttServer.WithDefaultEndpointPort(1884))
             .AddMqttConnectionHandler()
             .AddConnections(); ;
 
@@ -14,15 +22,18 @@ builder.Services.AddHostedMqttServer(mqttServer => mqttServer.WithoutDefaultEndp
 //{
 //    builder.WithDefaultEndpointPort(1883);
 //    builder.WithEncryptedEndpointPort(8883);
-//    builder.with
-//});
+//    //builder.WithDefaultEndpointBoundIPAddress(new IPAddress();
+//})
+//    .AddMqttConnectionHandler()
+//            .AddConnections(); 
+
 //// This adds TCP server support based on System.Net.Socket
-//builder.Services.AddMqttTcpServerAdapter();
+builder.Services.AddMqttTcpServerAdapter();
 
 //// This adds websocket support
-//builder.Services.AddMqttWebSocketServerAdapter();
+builder.Services.AddMqttWebSocketServerAdapter();
 
-//builder.Services.AddMqttConnectionHandler();
+builder.Services.AddMqttConnectionHandler();
 
 var app = builder.Build();
 
