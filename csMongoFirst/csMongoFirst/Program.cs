8c122870fe2fb0ffb2472a015fccac84b86b8374
Vulcan <vulcan.lee@gmail.com>
2022-10-24 11:33:13 +0800

    第一次使用 C# 來存取 NoSQL 的 MongoDB 資料庫

---
+++ csMongoFirst/csMongoFirst/Program.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace csMongoFirst;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var settings = MongoClientSettings
+            .FromConnectionString(
+            "mongodb+srv://vulcan:P%40ssw0rd@vulcanmongo.hptf95d.mongodb.net/?retryWrites=true&w=majority");
+        var client = new MongoClient(settings);
+        var database = client.GetDatabase("sample_mflix");
+        var collection = database.GetCollection<BsonDocument>("movies");
+        var result = collection.Find("{title:'The Princess Bride'}").FirstOrDefault();
+        Console.WriteLine(result);
+    }
+}
