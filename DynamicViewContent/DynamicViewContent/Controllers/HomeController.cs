ecdce0452a7f0e968b874b356b5eebfb134ef015
Vulcan <vulcan.lee@gmail.com>
2022-12-22 06:50:09 +0800

    測試 ASP.NET MVC 動態 cshtml

---
+++ DynamicViewContent/DynamicViewContent/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DynamicViewContent.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            Random random = new Random();
+            string viewIndex = random.Next(1, 2).ToString("D3");
+            string viewName = $"~/Views/Dynamic/View{viewIndex}.cshtml";
+            return View(viewName);
+            //return View();
+        }
+
+        public ActionResult Dynamic(int id)
+        {
+            ViewBag.Message = "Your contact page.";
+
+            string viewIndex = id.ToString("D3");
+            string viewName = $"~/Views/Dynamic/View{viewIndex}.cshtml";
+            return View(viewName);
+            //return View();
+        }
+    }
+}
