8a0d0fe9c3e31a8f1a943db946e0c60d639b4941
Vulcan <vulcan.lee@gmail.com>
2022-12-30 11:03:14 +0800

    .NET 6 Blazor Server template

---
+++ MyFirstMongoDB/MyFirstMongoDB/MagicHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstMongoDB;
+
+public static class MagicHelper
+{
+    public static readonly string ConnectionString = "mongodb+srv://vulcan:pass123@vulcanmongo.hptf95d.mongodb.net/?retryWrites=true&w=majority";
+    public static readonly string LabDatabase = "MyCRUD";
+    public static readonly string LabCollection = "Todo";
+}
---
+++ MyFirstMongoDB/MyFirstMongoDB/Todo.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstMongoDB
+{
+    [BsonIgnoreExtraElements]
+    public class Todo
+    {
+        [BsonId]
+        public ObjectId _id { get; set; }
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Completion { get; set; }
+        public List<int> Tags { get; set; }
+        public User User { get; set; }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+    }
+}
